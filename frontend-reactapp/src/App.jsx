26e320d3ce5e1ed7c390325005866580d45f7088
vignesh <vigneshpoluru1277@gmail.com>
2025-10-18 10:37:12 +0530

    Updated

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
